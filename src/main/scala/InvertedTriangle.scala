06e13fa9ac61630c1638e737edd58a45a1ad0998
saika <sxp42750@ucmo.edu>
2024-08-21 10:24:33 -0400

    Inverted Triangle Pattern

---
+++ src/main/scala/InvertedTriangle.scala
@@ -0,0 +1,12 @@
+object InvertedTriangle {
+  def main(args: Array[String]): Unit = {
+    for(i<-5 to 1 by -1){
+      for(j<-1 to i ){
+        print("*" + " ")
+
+      }
+      println()
+
+    }
+  }
+}
